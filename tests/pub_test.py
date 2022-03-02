f22b3274ba90e77e5c22172ec6194a7f14c96058
elicodes89 <elicodes89@gmail.com>
2022-03-02 12:18:39 +0000

    tested if pub has drinks

--- tests/pub_test.py
+++ tests/pub_test.py
@@ -3,10 +3,16 @@ from src.pub import Pub
 
 class TestPub(unittest.TestCase):
     def setUp(self):
-        self.pub = Pub("The name of the Pub", 100)
+        self.pub = Pub("The name of the Pub", 100, ["wine", "beer", "guiness"])
+        
 
     def test_pub_has_name(self):
         self.assertEqual("The name of the Pub", self. pub.name)
 
     def test_pub_has_a_till(self):
         self.assertEqual(100, self.pub.till)
+
+    def test_pub_has_drinks(self):
+        self.assertEqual(["wine", "beer", "guiness"], self.pub.drinks)
+
+    
