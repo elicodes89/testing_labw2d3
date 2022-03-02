06cab663a598004fc336fd8aa8f4a10504e7abbf
elicodes89 <elicodes89@gmail.com>
2022-03-02 13:51:42 +0000

    testing if customer has a wallet

--- tests/customer_test.py
+++ tests/customer_test.py
@@ -3,7 +3,12 @@ from src.customer import Customer
 
 class TestCustomer(unittest.TestCase):
     def setUp(self):
-        self.customer = Customer("The name of the Customer", 300)
+        self.customer = Customer("The name of the Customer", 100)
 
     def test_customer_has_name(self):
         self.assertEqual("The name of the Customer", self.customer.name)
+
+    def test_customer_has_wallet(self):
+        self.assertEqual(100, self.customer.wallet)
+
+    
