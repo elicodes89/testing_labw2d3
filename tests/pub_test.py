60ae4888e0e9e9679acba1cf43c64bc8b62231e2
elicodes89 <elicodes89@gmail.com>
2022-03-02 12:08:46 +0000

    created folders and testes if pub has a name

---
+++ tests/pub_test.py
@@ -0,0 +1,9 @@
+import unittest
+from src.pub import Pub 
+
+class TestPub(unittest.TestCase):
+    def setUp(self):
+        self.pub = Pub("The name of the Pub", 100)
+
+    def test_pub_has_name(self):
+        self.assertEqual("The name of the Pub", self. pub.name)
